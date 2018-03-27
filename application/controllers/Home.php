d3644ca3fe37039647eca17a0c2896bce902346a
Narendra <narry051@gmail.com>
2018-03-27 22:57:24 +0530

    werwer
    rewerwe

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function index() {
         $this->load->view('home/home');
     }
